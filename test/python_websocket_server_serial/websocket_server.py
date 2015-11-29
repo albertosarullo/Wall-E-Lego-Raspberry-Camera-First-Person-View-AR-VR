23ccef44a617827ddc49eba90aa1f001d327e05b
Alberto Sarullo <alberto.sarullo@gmail.com>
2015-11-29 22:23:13 +0100

    Add websocket for test head servos

---
+++ test/python_websocket_server_serial/websocket_server.py
@@ -0,0 +1,35 @@
+import threading
+import serial
+from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer
+
+serial_port = "/dev/cu.usbmodem1421";
+serial_baudrate = 9600
+websocket_port = 9000
+
+class TestWebSocket(WebSocket):
+  def handleMessage(self):
+    print "handleMessage"
+    arduino.write(str(self.data) + '\n')
+
+  def handleConnected(self):
+    print self.address, 'connected'
+
+  def handleClose(self):
+    print self.address, 'closed'
+
+def startWebSocketServer():
+  socketServer = SimpleWebSocketServer('', websocket_port, TestWebSocket)
+  socketServer.serveforever()
+
+if __name__ == "__main__":
+  
+  arduino = serial.Serial(serial_port, serial_baudrate, timeout=500, dsrdtr=False)
+  arduino.setDTR(True)
+  
+  threadWebSocket = threading.Thread(target=startWebSocketServer)
+  threadWebSocket.daemon = True
+  threadWebSocket.start()
+
+  while True:
+    time.sleep(1)
+
